2de6531d0d5fa18deecc109270dfdc26d41fe453
SaugaMgr <saugatmagar433@gmail.com>
2024-04-09 15:44:20 +0545

    user urls path changed to accounts/ and media url added to urlpatterns

--- core/Ecommerce/urls.py
+++ core/Ecommerce/urls.py
@@ -22,8 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('user/', include('apps.user.urls')),
+    path('accounts/', include('apps.user.urls')),
     path('', include('apps.product.urls')),
 ]
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
