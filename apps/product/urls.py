1bddc59e7daf29101a12e9bec0fbcf6142e62a49
SaugaMgr <saugatmagar433@gmail.com>
2024-04-09 20:32:24 +0545

    file format and search view url

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,12 +1,29 @@
 from django.urls import path
 
-from .views import HomePageView, AddProductView, ProductDetailView, UpdateProductView, ProductDeleteView, LikeProductView
+from .views import (
+    HomePageView,
+    AddProductView,
+    ProductDetailView,
+    UpdateProductView,
+    ProductDeleteView,
+    LikeProductView,
+    SearchResultsView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("product/add-product/", AddProductView.as_view(), name="add-product"),
-    path('product/like-product/', LikeProductView.as_view(), name='like-product'),
-    path('product/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
-    path('product/<slug:slug>/update/', UpdateProductView.as_view(), name='update-product'),
-    path('product/<slug:slug>/delete/', ProductDeleteView.as_view(), name='delete-product'),
+    path("product/like-product/", LikeProductView.as_view(), name="like-product"),
+    path("product/<slug:slug>/", ProductDetailView.as_view(), name="product-detail"),
+    path(
+        "product/<slug:slug>/update/",
+        UpdateProductView.as_view(),
+        name="update-product",
+    ),
+    path(
+        "product/<slug:slug>/delete/",
+        ProductDeleteView.as_view(),
+        name="delete-product",
+    ),
+    path("search/", SearchResultsView.as_view(), name="search"),
 ]
