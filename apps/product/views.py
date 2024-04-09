261a870504ab6dc21c1aa2449e80fbf1cbcf8ea9
SaugaMgr <saugatmagar433@gmail.com>
2024-04-09 19:25:38 +0545

    like product views and urls

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.views import View
 from django.views.generic import TemplateView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -44,3 +46,58 @@ class ProductDeleteView(DeleteView):
     template_name = 'main/home/product/delete_product.html'
     success_url = reverse_lazy('home')
     context_object_name = 'product'
+    
+class LikeProductView(View):
+    def post(self, request, *args, **kwargs):
+        product_slug = request.POST.get('product_slug')
+        product = Product.objects.get(slug=product_slug)
+        user = request.user
+
+        if product.liked_by.filter(id=user.id).exists():
+            product.liked_by.remove(user)
+            product.likes -= 1
+        else:
+            product.liked_by.add(user)
+            product.likes += 1
+        product.save()
+
+        return HttpResponseRedirect(reverse_lazy('home'))
+        
+        
+# class LikeProductView(View):
+#     def post(self, request, *args, **kwargs):
+#         import pdb
+
+#         pdb.set_trace()
+#         ajax_format = request.headers.get("x-requested-with")
+#         product_slug = request.POST.get("product_slug")
+#         product = Product.objects.get(slug=product_slug)
+#         user = request.user
+
+#         if ajax_format == "XMLHttpRequest":
+#             if product.liked_by.filter(id=user.id).exists():
+#                 product.liked_by.remove(user)
+#                 return JsonResponse(
+#                     {
+#                         "success": True,
+#                         "message": f"You disliked {product.name}.",
+#                     },
+#                     status=201,
+#                 )
+#             else:
+#                 product.liked_by.add(user)
+#                 return JsonResponse(
+#                     {
+#                         "success": True,
+#                         "message": f"You liked {product.name}.",
+#                     },
+#                     status=201,
+#                 )
+#         else:
+#             return JsonResponse(
+#                 {
+#                     "success": False,
+#                     "message": "Cannot process.Must be and AJAX XMLHttpRequest.",
+#                 },
+#                 status=400,
+#             )
