ca46336cb59590153ba2e39430b5d39cfd2ecfae
SaugaMgr <saugatmagar433@gmail.com>
2024-04-09 16:28:37 +0545

    using custom signup form to signup with email field

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+
+from .forms import SignUpForm
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
